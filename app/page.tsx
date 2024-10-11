a0fdccb7bff337e7f4ef6cd169e4cbf40a3ede34
Marlond <marlondcriollo@gmail.com>
2024-10-11 14:48:04 -0400

    kinda stable

--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,10 @@ import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { useState, FormEvent, use } from "react";
-import { stringify } from "querystring";
+import { useState, FormEvent } from "react";
 
 export default function Home() {
-  const testcases: string[] = [
-    `
-testcase
-
-    `,
-  ];
-  const templateVariables: string[] = [
+  const templateVariables = [
     "company_name",
     "support_email",
     "customer_email",
@@ -38,18 +31,11 @@ testcase
     "ordered_at",
     "address",
   ];
-  const expectedOutput: string[] = [];
 
   const [text, setText] = useState("");
   const [output, setOutput] = useState<any>();
   const [outputSet, setOutputSet] = useState(false);
   const [loading, setLoading] = useState(false);
-
-  const PrettyPrintJson = ({ data }: any) => (
-    <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
 
   const handleSubmit = async () => {
     try {
@@ -61,49 +47,60 @@ testcase
         },
         body: JSON.stringify({ emails: text, template: templateVariables }),
       });
-      console.log("Submit button clicked");
 
       const data = await response.json();
       setOutput(JSON.parse(data.responseContent));
       setOutputSet(true);
       setLoading(false);
-
-      console.log(JSON.parse(data.responseContent));
     } catch (error) {
       console.error(error);
+      setLoading(false);
     }
   };
 
   return (
-    <div className="p-5 flex flex-row">
-      <Card>
-        <CardHeader></CardHeader>
-        <CardContent>
-          <div className="flex items-center min-h-48 justify-center">
-            <Textarea
-              placeholder="Put emails here"
-              value={text}
-              className="flex min-h-48 items-center justify-center"
-              onChange={(e) => setText(e.target.value)}
-            ></Textarea>
-          </div>
-          {/* <div className=" bg-yellow-200">
-            {outputSet && (
-              <div className=" bg-cyan-200">
-                <Label>Output</Label>
-                <pre>{JSON.stringify(output, null, 2)}</pre>
-              </div>
-            )}
-          </div> */}
-          {loading && <div className="self-center">Loading...</div>}
-        </CardContent>
-        <CardFooter>
-          <Button onClick={handleSubmit}>Submit{outputSet}</Button>
-        </CardFooter>
-      </Card>
-      <Card>
-        <CardContent>ads</CardContent>
-      </Card>
+    <div className="h-screen p-5">
+      <div className="pb-5">
+        <Card className="">
+          <CardContent>temp</CardContent>
+        </Card>
+      </div>
+      <div className="grid grid-cols-2 gap-4 h-[calc(100vh-2.5rem)]">
+        <Card className="flex flex-col h-3/4">
+          <CardHeader className="flex-none">
+            <CardTitle>Input</CardTitle>
+          </CardHeader>
+          <CardContent className="flex-grow pt-2">
+            <div className="">
+              <Textarea
+                placeholder="Put emails here"
+                value={text}
+                className="min-h-80"
+                onChange={(e) => setText(e.target.value)}
+              />
+            </div>
+            {loading && <div className="mt-4">Loading...</div>}
+          </CardContent>
+          <CardFooter className="flex-none">
+            <Button onClick={handleSubmit}>Submit</Button>
+          </CardFooter>
+        </Card>
+
+        <Card className="flex flex-col h-3/4 overflow-auto">
+          <CardHeader className="flex-none">
+            <CardTitle>Output</CardTitle>
+          </CardHeader>
+          <CardContent className="flex overflow-scroll">
+            <div className="">
+              {outputSet && (
+                <pre className="whitespace-pre-wrap">
+                  {JSON.stringify(output, null, 2)}
+                </pre>
+              )}
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
